91c07f3a6fef9dd6c4b1fd5c6c5258403a209fac
Taynad <166086044+Taynad@users.noreply.github.com>
2025-08-09 17:14:36 -0300

    Create exerc01.js

---
+++ primeirosPassos/07-avaliação/exerc01.js
@@ -0,0 +1,7 @@
+/*
+    1) Faça um programa que receba a média de um aluno.
+        - Caso a média seja < 5 imprima "Reprovado"
+        - Caso a média seja >= 5 imprima "Recuperação"
+        - Caso a média seja >= 7 imprima "Aprovado"
+*/
+
